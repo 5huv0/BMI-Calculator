59d5c1dfeb0ccd99d4fa27d5231cbef15a24bcd4
5huv0 <md88748874@gmail.com>
2024-03-11 03:47:43 +0600

    js added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function calculate(){
+    const height = document.getElementById('height').value;
+    
+    const weight = document.getElementById('weight').value;
 
+    let calculateValue = weight / (height * height);
+
+    let final = Math.round(calculateValue);
+
+    document.getElementById('bmi-point').innerHTML = final;
+
+    if( final < 18.5){
+        document.getElementById('bmi-point').style.backgroundColor = 'rgb(188, 188, 11)';
+    }
+
+    else if( final > 18.5 && final < 25){
+        document.getElementById('bmi-point').style.backgroundColor = 'green';
+    }
+
+    else if( final > 25 && final < 30){
+        document.getElementById('bmi-point').style.backgroundColor = 'orangered';
+    }
+
+    else if( final > 30 ){
+        document.getElementById('bmi-point').style.backgroundColor = 'red';
+    }
 }
